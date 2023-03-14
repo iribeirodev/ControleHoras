e12f9392ad201b3a636994aa09d1763c896792f9
Itamar.Ribeiro <ribeirojr.itamar@gmail.com>
2023-03-14 09:49:27 -0300

    SetThreadExecutionState call

--- ControleHorasApp/FormPrincipal.cs
+++ ControleHorasApp/FormPrincipal.cs
@@ -1,17 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using ControleHorasApp.DAL;
-using ControleHorasApp.DTO;
 using ControleHorasApp.Services;
 
 namespace ControleHorasApp
 {
+
+
+    [FlagsAttribute]
+    public enum EXECUTION_STATE : uint
+    {
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+        ES_SYSTEM_REQUIRED = 0x00000001
+        // Legacy flag, should not be used.
+        // ES_USER_PRESENT = 0x00000004
+    }
+
     public partial class FormPrincipal : Form
     {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+
         public FormPrincipal()
         {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
             InitializeComponent();
         }
 
@@ -185,12 +202,14 @@ namespace ControleHorasApp
                 ExcluirTarefa();
         }
 
-        #endregion
-
         private void btnLog_Click(object sender, EventArgs e)
         {
             var frmLog = new FormLog();
             frmLog.ShowDialog();
         }
+
+        #endregion
+
+
     }
 }
